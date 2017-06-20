aa5d5f8c8a92666f1491c6f3b24454d3c01c9479
Dakota Wood <dwood32@sae.au>
2017-06-20 09:34:01 +0800

    Player Movement is in. Begun Packet setup.

---
+++ Client/ClientApp/Assets/Scripts/NetworkHandle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkHandle {
+
+    public struct GamePacket
+    {
+        int rotation; // 0 to 360
+        private Vector3 position; // Position
+    }
+
+}
