d6614d0c58e02c7ea54ab62cb0cc879a50222f29
Dakota Wood <dwood32@sae.au>
2017-06-29 14:18:41 +1000

    Added Memblock

--- Client/ClientApp/Assets/Scripts/NetworkHandle.cs
+++ Client/ClientApp/Assets/Scripts/NetworkHandle.cs
@@ -42,51 +42,20 @@ public class NetworkHandle : MonoBehaviour {
             else
                 rpc = FindObjectOfType<RemotePlayerController>();
         }
-
     }
 
     void TransmitPacket()
     {
         Boolean exception_thrown = false;
-        #region comments
-        // Create a socket object. This is the fundamental device used to network
-        // communications. When creating this object we specify:
-        // Internetwork: We use the internet communications protocol
-        // Dgram: We use datagrams or broadcast to everyone rather than send to
-        // a specific listener
-        // UDP: the messages are to be formated as user datagram protocal.
-        // The last two seem to be a bit redundant.
-        #endregion
-        Socket sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
-        ProtocolType.Udp);
-        #region comments 
-        // create an address object and populate it with the IP address that we will use
-        // in sending at data to. This particular address ends in 255 meaning we will send
-        // to all devices whose address begins with 192.168.2.
-        // However, objects of class IPAddress have other properties. In particular, the
-        // property AddressFamily. Does this constructor examine the IP address being
-        // passed in, determine that this is IPv4 and set the field. If so, the notes
-        // in the help file should say so.
-        #endregion
+
+        // Create the socket to send from
+        Socket sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        // Create an address object used to store the IP of the target
         IPAddress send_to_address = IPAddress.Parse("10.40.60.249");
-        #region comments
-        // IPEndPoint appears (to me) to be a class defining the first or final data
-        // object in the process of sending or receiving a communications packet. It
-        // holds the address to send to or receive from and the port to be used. We create
-        // this one using the address just built in the previous line, and adding in the
-        // port number. As this will be a broadcase message, I don't know what role the
-        // port number plays in this.
-        #endregion
+
+        // Create a target to send to
         IPEndPoint sending_end_point = new IPEndPoint(send_to_address, listenPort);
-        #region comments
-        // The below three lines of code will not work. They appear to load
-        // the variable broadcast_string witha broadcast address. But that
-        // address causes an exception when performing the send.
-        //
-        //string broadcast_string = IPAddress.Broadcast.ToString();
-        //Console.WriteLine("broadcast_string contains {0}", broadcast_string);
-        //send_to_address = IPAddress.Parse(broadcast_string);
-        #endregion
 
         //TODO: FORM PACKET HERE
         string text_to_send = "Bepis bepis in my conke";
@@ -126,16 +95,7 @@ public class NetworkHandle : MonoBehaviour {
         try
         {
             Debug.Log("Waiting for broadcast");
-            /*
-            // this is the line of code that receives the broadcase message.
-            // It calls the receive function from the object listener (class UdpClient)
-            // It passes to listener the end point groupEP.
-            // It puts the data from the broadcast message into the byte array
-            // named received_byte_array.
-            // I don't know why this uses the class UdpClient and IPEndPoint like this.
-            // Contrast this with the talker code. It does not pass by reference.
-            // Note that this is a synchronous or blocking call.
-            */
+
             if (listener.Available > 0)
             {
                 receive_byte_array = listener.Receive(ref groupEP);
